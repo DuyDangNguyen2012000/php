c48eaf1d4c831ccc424a70c1b94bd8bcc2be506a
DuyDangNguyen2012000 <nguyendangduy2012000@gmail.com>
2022-06-10 00:59:53 +0700

    update

--- resources/views/client/home.blade.php
+++ resources/views/client/home.blade.php
@@ -10,7 +10,7 @@
         <div class="row">
             <div class="col-lg-12">
                 <div class="section-title">
-                    <h2>Sản phẩms</h2>
+                    <h2>Sản phẩm</h2>
                 </div>
                 <div class="featured__controls">
                     <ul>
